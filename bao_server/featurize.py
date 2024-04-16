683755e0891b444ec4f55e0aee84b0404fcab40f
SergeyZinchenko <zinchse@Air-13-main.local>
2024-04-16 17:30:24 +0700

    first try commit

--- bao_server/featurize.py
+++ bao_server/featurize.py
@@ -9,11 +9,14 @@ class TreeBuilderError(Exception):
     def __init__(self, msg):
         self.__msg = msg
 
+
 def is_join(node):
     return node["Node Type"] in JOIN_TYPES
 
+
 def is_scan(node):
     return node["Node Type"] in LEAF_TYPES
+
 
 class TreeBuilder:
     def __init__(self, stats_extractor, relations):
@@ -29,7 +32,9 @@ class TreeBuilder:
             name_key = "Index Name" if "Index Name" in node else "Relation Name"
             if name_key not in node:
                 print(node)
-                raise TreeBuilderError("Bitmap operator did not have an index name or a relation name")
+                raise TreeBuilderError(
+                    "Bitmap operator did not have an index name or a relation name"
+                )
             for rel in self.__relations:
                 if rel in node[name_key]:
                     return rel
@@ -37,7 +42,7 @@ class TreeBuilder:
             raise TreeBuilderError("Could not find relation name for bitmap index scan")
 
         raise TreeBuilderError("Cannot extract relation type from node")
-                
+
     def __featurize_join(self, node):
         assert is_join(node)
         arr = np.zeros(len(ALL_TYPES))
@@ -48,8 +53,7 @@ class TreeBuilder:
         assert is_scan(node)
         arr = np.zeros(len(ALL_TYPES))
         arr[ALL_TYPES.index(node["Node Type"])] = 1
-        return (np.concatenate((arr, self.__stats(node))),
-                self.__relation_name(node))
+        return (np.concatenate((arr, self.__stats(node))), self.__relation_name(node))
 
     def plan_to_feature_tree(self, plan):
         children = plan["Plans"] if "Plans" in plan else []
@@ -68,21 +72,16 @@ class TreeBuilder:
             assert not children
             return self.__featurize_scan(plan)
 
-        raise TreeBuilderError("Node wasn't transparent, a join, or a scan: " + str(plan))
+        raise TreeBuilderError(
+            "Node wasn't transparent, a join, or a scan: " + str(plan)
+        )
+
 
 def norm(x, lo, hi):
     return (np.log(x + 1) - lo) / (hi - lo)
 
-def get_buffer_count_for_leaf(leaf, buffers):
-    total = 0
-    if "Relation Name" in leaf:
-        total += buffers.get(leaf["Relation Name"], 0)
 
-    if "Index Name" in leaf:
-        total += buffers.get(leaf["Index Name"], 0)
-
-    return total
-
+# @me: cmd pattern for log normalization
 class StatExtractor:
     def __init__(self, fields, mins, maxs):
         self.__fields = fields
@@ -98,56 +97,43 @@ class StatExtractor:
                 res.append(norm(inp[f], lo, hi))
         return res
 
+
+# @me: to add rel names; rm costs
 def get_plan_stats(data):
     costs = []
     rows = []
-    bufs = []
-    
-    def recurse(n, buffers=None):
+
+    def recurse(n):
         costs.append(n["Total Cost"])
         rows.append(n["Plan Rows"])
-        if "Buffers" in n:
-            bufs.append(n["Buffers"])
 
         if "Plans" in n:
             for child in n["Plans"]:
                 recurse(child)
 
     for plan in data:
-        recurse(plan["Plan"], buffers=plan.get("Buffers", None))
+        recurse(plan["Plan"])
 
     costs = np.array(costs)
     rows = np.array(rows)
-    bufs = np.array(bufs)
-    
+
+    # @me: to extract max log/ min log
     costs = np.log(costs + 1)
     rows = np.log(rows + 1)
-    bufs = np.log(bufs + 1)
 
     costs_min = np.min(costs)
     costs_max = np.max(costs)
     rows_min = np.min(rows)
     rows_max = np.max(rows)
-    bufs_min = np.min(bufs) if len(bufs) != 0 else 0
-    bufs_max = np.max(bufs) if len(bufs) != 0 else 0
 
-    if len(bufs) != 0:
-        return StatExtractor(
-            ["Buffers", "Total Cost", "Plan Rows"],
-            [bufs_min, costs_min, rows_min],
-            [bufs_max, costs_max, rows_max]
-        )
-    else:
-        return StatExtractor(
-            ["Total Cost", "Plan Rows"],
-            [costs_min, rows_min],
-            [costs_max, rows_max]
-        )
-        
+    return StatExtractor(
+        ["Total Cost", "Plan Rows"], [costs_min, rows_min], [costs_max, rows_max]
+    )
+
 
 def get_all_relations(data):
     all_rels = []
-    
+
     def recurse(plan):
         if "Relation Name" in plan:
             yield plan["Relation Name"]
@@ -158,53 +144,21 @@ def get_all_relations(data):
 
     for plan in data:
         all_rels.extend(list(recurse(plan["Plan"])))
-        
+
     return set(all_rels)
 
-def get_featurized_trees(data):
-    all_rels = get_all_relations(data)
-    stats_extractor = get_plan_stats(data)
 
-    t = TreeBuilder(stats_extractor, all_rels)
-    trees = []
-
-    for plan in data:
-        tree = t.plan_to_feature_tree(plan)
-        trees.append(tree)
-            
-    return trees
-
-def _attach_buf_data(tree):
-    if "Buffers" not in tree:
-        return
-
-    buffers = tree["Buffers"]
-
-    def recurse(n):
-        if "Plans" in n:
-            for child in n["Plans"]:
-                recurse(child)
-            return
-        
-        # it is a leaf
-        n["Buffers"] = get_buffer_count_for_leaf(n, buffers)
-
-    recurse(tree["Plan"])
-
+# @me: it is cmd pattern for prev; (there 'trees' == 'data')
 class TreeFeaturizer:
     def __init__(self):
         self.__tree_builder = None
 
     def fit(self, trees):
-        for t in trees:
-            _attach_buf_data(t)
         all_rels = get_all_relations(trees)
         stats_extractor = get_plan_stats(trees)
         self.__tree_builder = TreeBuilder(stats_extractor, all_rels)
 
     def transform(self, trees):
-        for t in trees:
-            _attach_buf_data(t)
         return [self.__tree_builder.plan_to_feature_tree(x["Plan"]) for x in trees]
 
     def num_operators(self):
